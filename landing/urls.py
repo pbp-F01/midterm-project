6a33f5bf5ff616dc0fb2f88dcabf576bf5c49042
Rafinal Haryokusumo Taloputra <rafinal.haryokusumo@ui.ac.id>
2022-10-28 21:44:08 +0700

    nambahin login.html

--- landing/urls.py
+++ landing/urls.py
@@ -4,5 +4,7 @@ from landing.views import *
 app_name = 'landing'
 
 urlpatterns = [
-    
+    path('', index, name='index'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
 ]
