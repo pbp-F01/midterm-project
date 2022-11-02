26d253dea41a31f5d62c3f3345ff70cee34578da
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2022-11-02 22:15:35 +0700

    model fix

--- landing/models.py
+++ landing/models.py
@@ -5,25 +5,10 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     roles = models.CharField(max_length=1)
-
-    def konsumen(self):
-        if self.roles == 'K':
-            return True
-        return False
-
-    def investor(self):
-        if self.roles == 'I':
-            return True
-        return False
-
-    def pemilik(self):
-        if self.roles == 'P':
-            return True
-        return False
 
     def __str__(self):
         return self.user.username
