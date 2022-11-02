cb5f83f54c056f89f90f989e3ab532d825a27088
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2022-11-02 23:54:10 +0700

    fix redirect

--- reviewUMKM/views.py
+++ reviewUMKM/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from requests import request
 from profileUMKM.models import ProfileUMKM
-from . models import Review 
+from . models import Review
 from . forms import ReviewForm
 from django.http import HttpResponse
 from django.core import serializers
+
 
 def home(request):
     items = ProfileUMKM.objects.all()
     print(items)
     context = {
-        'items':items
+        'items': items
     }
-    return render(request, "home.html",context)
+    return render(request, "/reviewUMKM/home.html", context)
+
 
 def rate(request, id):
     post = ProfileUMKM.objects.get(id=id)
@@ -21,24 +23,27 @@ def rate(request, id):
         author = request.POST.get('author')
         stars = request.POST.get('stars')
         comment = request.POST.get('comment')
-        review = Review(author = author, stars = stars,  comment=comment , umkm=post)
+        review = Review(author=author, stars=stars,
+                        comment=comment, umkm=post)
         review.save()
         return redirect('reviewUMKM:success')
 
     form = ReviewForm()
     context = {
-        "form":form
+        "form": form
 
     }
-    return render(request, 'rate.html',context)
+    return render(request, 'rate.html', context)
 
 
 def success(request):
     return render(request, "success.html")
 
+
 def show_json_by_id(request, id):
     ratingbyid = Review.objects.get(pk=id)
     return HttpResponse(serializers.serialize('json', [ratingbyid]), content_type='application/json')
+
 
 def show_rating_json(request):
     rating = Review.objects.all()
