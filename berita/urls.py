961d85a14ee3feb9da29d7cca30b670881a3f498
shafaleyda <shafaleyda@gmail.com>
2022-11-02 23:48:12 +0700

    final commit

--- berita/urls.py
+++ berita/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from berita.views import show_landing_news, show_news, show_json, show_json_comment, add_comment, show_url, show_profile, show_roles
+from berita.views import show_landing_news, show_news, show_json, show_json_comment, add_comment, show_url, show_profile
 
 app_name='berita'
 
@@ -11,5 +11,4 @@ urlpatterns = [
     path('news_page/add_comment/<int:id>', add_comment, name='add_comment'), 
     path('show_url', show_url, name='show_url'), 
     path('show_profile', show_profile, name='show_profile'), 
-    # path('show_roles', show_roles, name='show_roles')
 ]
