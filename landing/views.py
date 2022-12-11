ac999dcf7b771c0d52d09175a24903f32d4960c4
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2022-12-11 23:27:36 +0700

    auth register

--- landing/views.py
+++ landing/views.py
@@ -28,43 +28,6 @@ def logout_user(request):
     return response
 
 
-def register_flutter(request):
-    if request.method == 'POST':
-
-        data = json.loads(request.body)
-
-        full_name = data["full_name"]
-        email = data["email"]
-        password1 = data["password1"]
-        password2 = data["password2"]
-        status = data["status"]
-        username = data["username"]
-
-        if UserModel.objects.filter(username=username).exists():
-            return JsonResponse({"status": "duplicate"}, status=401)
-
-        if password1 != password2:
-            return JsonResponse({"status": "pass failed"}, status=401)
-
-        createUser = UserModel.objects.create_user(
-            username=username,
-            password=password1,
-        )
-
-        createUser.save()
-        newUser = Profile.objects.create(
-            user=createUser,
-            email=email,
-            status=status,
-            full_name=full_name
-        )
-
-        newUser.save()
-        return JsonResponse({"status": "success"}, status=200)
-    else:
-        return JsonResponse({"status": "error"}, status=401)
-
-
 def register(request):
     form = SignUp()
 
@@ -79,32 +42,6 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
-
-
-def login_flutter(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            auth_login(request, user)
-            # Redirect to a success page.
-            return JsonResponse({
-                "status": True,
-                "message": "Successfully Logged In!"
-                # Insert any extra data if you want to pass data to Flutter
-            }, status=200)
-        else:
-            return JsonResponse({
-                "status": False,
-                "message": "Failed to Login, Account Disabled."
-            }, status=401)
-
-    else:
-        return JsonResponse({
-            "status": False,
-            "message": "Failed to Login, check your email/password."
-        }, status=401)
 
 
 def login_user(request):
