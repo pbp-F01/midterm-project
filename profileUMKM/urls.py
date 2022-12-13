d730281bd1f4f8fd0261251408f672fd2b957234
recedivies <ahmadhiprananta@gmail.com>
2022-12-13 21:06:11 +0700

    remove auto seeds

--- profileUMKM/urls.py
+++ profileUMKM/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path("", views.list_profile_UMKM, name="list_profile_UMKM"),
     path("json/", views.list_profile_UMKM_json, name="list_profile_UMKM_json"),
     path("json/<int:pk>/", views.get_profile_UMKM_json, name="get_profile_UMKM_json"),
-    # path("add/", views.create_profile_UMKM, name="create_profile_UMKM"),
-    # path("delete/<int:pk>/", views.delete_profile_UMKM, name="delete_profile_UMKM"),
-    path("add/", views.create_profile_UMKM_flutter, name="create_profile_UMKM_flutter"),
-    path(
-        "delete/<int:pk>/",
-        views.delete_profile_UMKM_flutter,
-        name="delete_profile_UMKM_flutter",
-    ),
+    path("add/", views.create_profile_UMKM, name="create_profile_UMKM"),
+    path("delete/<int:pk>/", views.delete_profile_UMKM, name="delete_profile_UMKM"),
+    # path("add/", views.create_profile_UMKM_flutter, name="create_profile_UMKM_flutter"),
+    # path(
+    #     "delete/<int:pk>/",
+    #     views.delete_profile_UMKM_flutter,
+    #     name="delete_profile_UMKM_flutter",
+    # ),
 ]
