1d28a34234dd5e1834d45ce35b7e12adad539fc8
saniarizqi <sania.rizqi@ui.ac.id>
2022-12-14 13:51:58 +0700

    add views for flutter

--- penjualan/views.py
+++ penjualan/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import redirect
 from penjualan.models import Laporan
 from django.contrib.auth.decorators import login_required
 from landing.models import Profile
+from django.views.decorators.csrf import csrf_exempt
+from penjualan.forms import PenjualanForm
 
 # Create your views here.
 @login_required(login_url='/landing/login/')
@@ -37,3 +39,22 @@ def add_laporan(request):
                         
         laporan.save()
         return redirect('penjualan:show_dashboard')
+
+@csrf_exempt
+def add_laporan_flutter(request):
+    user = get_object_or_404(Profile, user=request.user)
+
+    if request.method == "POST" and user.roles == "P":
+        body = JsonResponse.loads(request.body.decode("utf-8"))
+        form = PenjualanForm(body)
+
+        if form.is_valid():
+            form.cleaned_data["user"] = user
+            data = form.cleaned_data
+            PenjualanForm.objects.create(**data)
+
+            return JsonResponse(
+                {"status": True, "message": "Berhasil menambahkan Profil UMKM!"}
+            )
+
+        return JsonResponse({"status": False, "message": form.errors})
